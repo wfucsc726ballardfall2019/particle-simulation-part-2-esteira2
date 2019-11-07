b507aff62ebb2d4587d6f941920b3a4a1891108e
Irina Viviano <viviiv14@a1a-u3-c3-b4.deac.wfu.edu>
2019-11-07 13:25:13 -0500

    Fixed serial code to be efficient

--- serial.cpp
+++ serial.cpp
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <vector>
 #include "common.h"
+
+using namespace std;
 
 //
 //  benchmarking program
@@ -34,6 +37,31 @@ int main( int argc, char **argv )
     particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
     set_size( n );
     init_particles( n, particles );
+
+    // Set the size of the bins
+    // The grid size is sqrt(0.0005 * number of particles)
+    // So make sure the bins only consider the cutoff radius where the particles actually react to each other
+    double bin_length = getCutoff() * 2;
+    int num_bins = ceil((sqrt(getDensity() * n)) / bin_length);
+
+    // Array to keep track of which particles are in which bins
+    vector<vector<particle_t> > bins(num_bins * num_bins);
+    int offset_x;
+    int offset_y;
+    int which_bin;
+
+    // Compute the bins
+    for (int i = 0; i < n; i++)
+    {
+        // Compute which bin a particle belongs to based on its location
+        offset_x = floor(particles[i].x / bin_length);
+        offset_y = floor(particles[i].y / bin_length);
+
+        which_bin = num_bins * offset_y + offset_x;
+
+        // Add the particle to the list of particles in that bin
+        bins[which_bin].push_back(particles[i]);
+    }
     
     //
     //  simulate a number of time steps
@@ -42,17 +70,33 @@ int main( int argc, char **argv )
 	
     for( int step = 0; step < NSTEPS; step++ )
     {
-	navg = 0;
+	    navg = 0;
         davg = 0.0;
-	dmin = 1.0;
+	    dmin = 1.0;
         //
         //  compute forces
         //
         for( int i = 0; i < n; i++ )
         {
             particles[i].ax = particles[i].ay = 0;
-            for (int j = 0; j < n; j++ )
-				apply_force( particles[i], particles[j],&dmin,&davg,&navg);
+
+            // Get the bin of the current particle
+            offset_x = floor(particles[i].x / bin_length);
+            offset_y = floor(particles[i].y / bin_length);
+
+            // Make sure the x position doesn't go beyond 0 to num_bins - 1
+            for (int x = max(0, offset_x - 1); x <= min(offset_x + 1, num_bins - 1); x++) {
+                // Make sure the y position doesn't go beyond 0 to num_bins - 1
+                for (int y = max(0, offset_y - 1); y <= min(offset_y + 1, num_bins - 1); y++) {
+                    // Now compute which bin we are currently considering for our force computation
+                    which_bin = num_bins * y + x;
+                    // Consider each particle in that bin 
+                    for (int p = 0; p < bins[which_bin].size(); p++) {
+                        // Compute the force between the current particle and the particles in this bin
+                        apply_force(particles[i], bins[which_bin][p], &dmin, &davg, &navg);
+                    }
+                }
+            }
         }
  
         //
@@ -77,6 +121,23 @@ int main( int argc, char **argv )
           //
           if( fsave && (step%SAVEFREQ) == 0 )
               save( fsave, n, particles );
+        }
+
+        // The particles have moved, so update the particles in each bin
+        // First, clear the current bin information
+        for (int i = 0; i < num_bins * num_bins; i++) 
+            bins[i].resize(0);
+
+        // Update
+        for (int i = 0; i < n; i++)
+        {
+            // Compute which bin a particle belongs to based on its location
+            offset_x = floor(particles[i].x / bin_length);
+            offset_y = floor(particles[i].y / bin_length);
+
+            which_bin = num_bins * offset_y + offset_x;
+
+            bins[which_bin].push_back(particles[i]);
         }
     }
     simulation_time = read_timer( ) - simulation_time;
